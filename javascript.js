5c5e0995d07b6581c4ea3f17a7de5c37de15fedc
NotaVaubanmain <HyakkidoKaen@protonmail.com>
2025-09-03 21:33:18 +1000

    proto22

--- javascript.js
+++ javascript.js
@@ -1,25 +1,49 @@
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
+let humanScore = 0
+let computerScore = 0
+
 
 function getComputerChoice(getHumanChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     console.log(computerChoice)
     }   
-getComputerChoice()    
+  
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, Scissors")
+function getHumanChoice(getComputerChoice) {
+    let humanChoice = prompt("Rock, Paper, Scissors").toLowerCase;
 
-    if (humanChoice === "Rock") {
+    if (humanChoice ==="rock") {
         console.log("Rock")
-    } else if (humanChoice === "Paper") {
+    } else if (humanChoice ==="paper") {
         console.log("Paper")
-    } else if (humanChoice === "Scissors") {
+    } else if (humanChoice ==="scissors") {
         console.log("Scissors")
     } else {
         console.log("MAKE UR CHOICE")
     }
 }
-getHumanChoice()
+
+
+function playRound(humanChoice , computerChoice) {
+    if (humanSelection === "Rock" && computerSelection === "Paper") {
+    console.log("You Lose!")
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    console.log("You Win!")    
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You Win!")
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("You Lose!")
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You Lose!")
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You Win!")
+    }
+} 
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection);
+
 // Create function 
 // use math.random to randomly return 
 
